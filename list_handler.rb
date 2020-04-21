4b2245e8095db972ffac0e5b54f4a43e3cfb4aa8
Joseph Miller <joff@joff.codes>
2020-04-21 17:45:48 +1000

    Add a GET endpoint

---
+++ list_handler.rb
@@ -0,0 +1,26 @@
+require 'json'
+require_relative 'lib/concierge'
+
+class ListHandler
+
+  def self.list_reservations(event:, context:)
+    puts event
+
+    {statusCode: 200, body: reservations.to_json}
+  end
+
+  private
+
+  def self.reservations
+    Concierge.new.reservations.map { |r| present_reservation(r) }
+  end
+
+  def self.present_reservation(reservation)
+    {
+      environment: reservation.environment,
+      user: reservation.user_name,
+      reason: reservation.comment
+    }
+  end
+
+end
